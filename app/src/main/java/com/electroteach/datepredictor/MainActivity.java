fe0370cdbd1b386037b1016e0583ed328224569e
MatrixMike <matroxmike@icloud.com>
2021-05-06 17:12:03 +1000

    revived on OPTI D

--- app/src/main/java/com/electroteach/datepredictor/MainActivity.java
+++ app/src/main/java/com/electroteach/datepredictor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.electroteach.datepredictor;
-// Thu Jul 09 22:45:15 AEST 2020 
+// Thu Jul 09 22:45:15 AEST 2020
+// Saturday 10 April  14:03:12 AEST 2021
 // DatePredictor
 
 import android.os.Bundle;
@@ -11,7 +12,13 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+/*
+TODO    java.lang.RuntimeException:
+    Unable to start activity ComponentInfo{com.electroteach.datepredictor/com.electroteach.datepredictor.MainActivity}:
+     java.lang.IllegalStateException:
+      Activity com.electroteach.datepredictor.MainActivity@ff6aab7 does not have a NavController set on 2131230987
 
+ */
 public class MainActivity extends AppCompatActivity {
     Integer x;
 
