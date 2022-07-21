8dfcc4b948602d6f2820d877ecb27bc93f1ba8fa
MichaelH <mh6269@gmail.com>
2022-07-21 15:03:17 +1000

    Update HomeFragment.java

--- app/src/main/java/com/electroteach/datepredictor/ui/home/HomeFragment.java
+++ app/src/main/java/com/electroteach/datepredictor/ui/home/HomeFragment.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -22,12 +21,7 @@ public class HomeFragment extends Fragment {
         HomeViewModel homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
@@ -35,15 +29,12 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_home).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                HomeFragmentDirections.ActionHomeFragmentToHomeSecondFragment action =
-                        HomeFragmentDirections.actionHomeFragmentToHomeSecondFragment
-                                ("From HomeFragment");
-                NavHostFragment.findNavController(HomeFragment.this)
-                        .navigate(action);
-            }
+        view.findViewById(R.id.button_home).setOnClickListener(view1 -> {
+            HomeFragmentDirections.ActionHomeFragmentToHomeSecondFragment action =
+                    HomeFragmentDirections.actionHomeFragmentToHomeSecondFragment
+                            ("From HomeFragment");
+            NavHostFragment.findNavController(HomeFragment.this)
+                    .navigate(action);
         });
     }
 }
